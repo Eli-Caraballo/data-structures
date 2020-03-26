15bf82f5f7c69c71fe5debefa8b3947941a5c3c8
Hieu Ho <hihho@ucdavis.edu>
2020-03-26 09:22:57 -0700

    Completed queue with prototypal

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -2,20 +2,18 @@ var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
   someInstance.incrementor = 0;
   someInstance.topQ = 0;
   return someInstance;
 };
 
-Queue.queueMethods = {};
-
-  Queue.prototype.enqueue = function(value) {
+var queueMethods = {
+  enqueue(value) {
     this.storage[this.incrementor] = value;
     this.incrementor++;
-    this.
-  };
-
-  Queue.prototype.dequeue = function() {
+  },
+  dequeue() {
     var cache;
     if (this.storage[this.topQ]) {
       cache = this.storage[this.topQ];
@@ -23,11 +21,9 @@ Queue.queueMethods = {};
       this.topQ++;
     }
     return cache;
-  };
-
-  Queue.prototype.size = function() {
+  },
+  size() {
     var length = Object.keys(this.storage);
     return length.length;
-  };
-
-
+  }
+};
