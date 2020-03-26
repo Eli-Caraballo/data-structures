4ca17b10d1be776cb935af5de9adff3fef6ead43
Hieu Ho <hihho@ucdavis.edu>
2020-03-26 10:11:37 -0700

    Added stack function using prototypal

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  someInstance.incremetor = 1;
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push(value) {
+    this.storage[this.incremetor] = value;
+    this.incremetor++;
+  },
+  pop() {
+    this.incremetor--;
+    var cache = this.storage[this.incremetor];
+    delete this.storage[this.incremetor];
+    return cache;
+  },
+  size() {
+    return Object.keys(this.storage).length;
+  }
+};
 
 
