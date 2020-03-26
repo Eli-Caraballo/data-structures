2bdf225d7d6d4550b184ac484cc9e660926a8607
Hieu Ho <hihho@ucdavis.edu>
2020-03-25 19:04:03 -0700

    completed queue functional shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,35 +1,41 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var storage = {};
-  storage.enqueue = 0;
-  storage.dequeue = 0;
-  storage.size = 0;
-  _.extend(storage, Queue.queueMethods);
-  return storage;
+  var someInstance = {
+    storage: {},
+    incrementer: 0,
+    decrementer: 0,
+  };
+  extend(someInstance, queueMethods);
+  return someInstance;
 };
 
-// var queueMethods = {};
-
-Queue.queueMethods = {};
-
-Queue.queueMethods.enqueue = function(value) {
-  var incrementer = 1;
-  this.incrementer = value;
-  incrementer++;
-};
-
-Queue.queueMethods.dequeue = function() {
-  var topQ = 1;
-  var value;
-  if(this[topQ]){
-    value = this[topQ];
-    delete this[topQ];
-    topQ++;
+var extend = function(destination, add) {
+  for (var key in add) {
+    destination[key] = add[key];
   }
-  return value;
 };
 
-Queue.queueMethods.size = function() {
-  return _.size(Queue.queueMethods);
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.incrementer] = value;
+    this.incrementer++;
+  },
+
+  dequeue: function() {
+    var value;
+    if (this.storage[this.decrementer]) {
+      value = this.storage[this.decrementer];
+      delete this.storage[this.decrementer];
+      this.decrementer++;
+    }
+    return value;
+  },
+
+  size: function() {
+    var arr = Object.keys(this.storage);
+    return arr.length;
+  }
 };
+
+
