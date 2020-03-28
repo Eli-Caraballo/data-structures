90196eb4630a1d1617a8afb9084bc37047742adb
Eli Caraballo <eli.caraballojr@gmail.com>
2020-03-28 13:59:08 -0500

    Completed Binary Search Tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,56 @@
 var BinarySearchTree = function(value) {
-  
+  var branch = {};
+  branch['value'] = value;
+  branch['left'];
+  branch['right'];
+  _.extend(branch, binaryTreeMethods);
+  return branch;
 };
 
+var binaryTreeMethods = {};
+
+binaryTreeMethods.insert = function(value) {
+  if (this['value'] > value) {
+    if (this.left !== undefined) {
+      this.left.insert(value);
+    } else {
+      this.left = BinarySearchTree(value);
+    }
+  } else {
+    if (this.right !== undefined) {
+      this.right.insert(value);
+    } else {
+      this.right = BinarySearchTree(value);
+    }
+  }
+};
+
+binaryTreeMethods.contains = function(value) {
+  if (_.contains(this, value)) {
+    return true;
+  }
+
+  if (this['value'] > value) {
+    if (this.left !== undefined) {
+      return this.left.contains(value);
+    }
+  } else {
+    if (this.right !== undefined) {
+      return this.right.contains(value);
+    }
+  }
+  return false;
+};
+
+binaryTreeMethods.depthFirstLog = function(func) {
+  func(this.value);
+  if (this.left !== undefined) {
+    this.left.depthFirstLog(func);
+  }
+  if (this.right !== undefined) {
+    this.right.depthFirstLog(func);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
